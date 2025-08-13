88d1f02fbe68d0858d0411a3d19661dbe94a423f
ruben <ruben@Mini-de-ruben.home>
2025-08-13 07:59:52 +0200

    refactor: Improve application startup and orchestration engine initialization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from src.core.api_handler import app
+from src.core.api_handler import create_app
 
 if __name__ == "__main__":
+    app = create_app()
     uvicorn.run(app, host="0.0.0.0", port=8000)
